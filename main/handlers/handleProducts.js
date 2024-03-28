722cea4b5d4c1fca93ae1cc664c795a367157483
demczenko <134509169+demczenko@users.noreply.github.com>
2024-03-28 13:11:33 +0100

    Delete main/handlers/handleProducts.js

--- main/handlers/handleProducts.js
+++
@@ -1,7 +0,0 @@
-import { addParamsProduct } from "../../helpers/addQueryParams.js";
-import { parsePrice } from "../../helpers/parsePrice.js";
-
-export function handleProducts() {
-  parsePrice();
-  addParamsProduct();
-}
